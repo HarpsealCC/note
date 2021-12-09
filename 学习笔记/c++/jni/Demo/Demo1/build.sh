059a3490be05cb31ee5400c2b9932feadf399352
lcz <lcz@git.com>
2021-12-09 14:18:46 +0800

    为c++编译的环境变量在jni的Demo1编译脚本做个demo

--- 学习笔记/c++/jni/Demo/Demo1/build.sh
+++ 学习笔记/c++/jni/Demo/Demo1/build.sh
@@ -8,3 +8,8 @@ javac *.java
 # export JRE_HOME = JAVA_HOME or ${JAVA_HOME}/jre  
 # export LD_LIBRARY_PATH=${JRE_HOME}/lib/server
 g++ -o testjava TestJNI.cpp -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -L${JRE_HOME}/lib/server -ljvm 
+
+# 下面是将头文件与动态库添加到环境变量中的写法，执行时候需要添加LD_LIBRARY_PATH
+# export LIBRARY_PATH=${JRE_HOME}/lib/server
+# export CPLUS_INCLUDE_PATH=${JAVA_HOME}/include:${JAVA_HOME}/include/linux
+# g++ -o testjava TestJNI.cpp -ljvm
