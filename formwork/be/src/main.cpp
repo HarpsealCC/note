dedec103078503f1b212445912a7e5a33826f3a7
lcz <lcz@git.com>
2024-09-22 12:30:42 +0800

    [docs](算法&学习笔记) 新增算法文件夹用于记录左程云学习笔记与其他算法

--- formwork/be/src/main.cpp
+++ formwork/be/src/main.cpp
@@ -1,40 +1,35 @@
+#include <functional>
 #include <iostream>
-#include <mutex>
-#include <thread>
+#include <vector>
+class bloomfilter {
+public:
+    bloomfilter(int size, int hash_size) : size_(size), hash_size_(hash_size) {
+        bv_.resize(size_, 0);
+    }
 
-std::mutex mutex1;
-std::mutex mutex2;
+    void insert(const std::string& str) {
+        for (int i = 0; i < size_; i++) {
+            bv_[hasher(str, i) % size_] = 1;
+        }
+    }
 
-void thread1() {
-    std::lock_guard<std::mutex> lock1(mutex1);
-    std::cout << "Thread 1 acquired lock on mutex1" << std::endl;
+    bool contain(const std::string& str) {
+        for (int i = 0; i < size_; i++) {
+            if (bv_[hasher(str, i) % size_] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-    // 模拟一些工作
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+private:
+    int hasher(const std::string str, int seed) {
+        std::hash<std::string> hasher;
+        return hasher(str) + seed * 0x123456;
+    }
 
-    std::cout << "Thread 1 waiting for lock on mutex2" << std::endl;
-    std::lock_guard<std::mutex> lock2(mutex2);  // 这里等待 mutex2，死锁发生
-    std::cout << "Thread 1 acquired lock on mutex2" << std::endl;
-}
-
-void thread2() {
-    std::lock_guard<std::mutex> lock2(mutex2);
-    std::cout << "Thread 2 acquired lock on mutex2" << std::endl;
-
-    // 模拟一些工作
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-    std::cout << "Thread 2 waiting for lock on mutex1" << std::endl;
-    std::lock_guard<std::mutex> lock1(mutex1);  // 这里等待 mutex1，死锁发生
-    std::cout << "Thread 2 acquired lock on mutex1" << std::endl;
-}
-
-int main() {
-    std::thread t1(thread1);
-    std::thread t2(thread2);
-
-    t1.join();
-    t2.join();
-
-    return 0;
-}
+private:
+    int                 size_;
+    int                 hash_size_;
+    std::vector<int8_t> bv_;
+};
